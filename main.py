419aac9621857c1a2c12f523fe0fa7ff86287084
Daniel Ivánek <ivanek.daniel99@gmail.com>
2023-07-30 21:21:21 +0200

    commit

--- main.py
+++ main.py
@@ -1,67 +1,22 @@
 """
 projekt_2.py: 
-
 author: Daniel Ivánek   
 email: ivanek.daniel99@gmail.com
-discord: Notme#1202
+discord: Notme#1275
 """
-import random
+
 import test
+import uuid
 line = "-" * 30
-
-
-
-test.uvitani()
-
-def generate_unique_four_digit_number():
-    # Generate a random permutation of digits 1 to 9
-    digits = random.sample(range(1, 10), 4)
-
-    # Convert the list of digits to a 4-digit integer
-    ran_number = digits[0] * 1000 + digits[1] * 100 + digits[2] * 10 + digits[3]
-
-    return ran_number
-
-generated_num = generate_unique_four_digit_number()
-
-def correct_number_check():
-    smycka = True
-    while smycka:
-        players_number = input("Guess the number: ")
-        if not players_number.isdigit():
-            print("You entered non-numeric value.")
-            continue
-        elif str(players_number[0]) == "0":
-            print("Number starts with 0.")
-            continue
-        elif len(set(list(str(players_number)))) != 4:
-            print("Number has less/more than 4 digits or includes duplicates")
-        else:
-            smycka = False
-   
-    print("hope it works")
-    return players_number
-
-
+try_count = 0
 x_count = 0
 y_count = 0
 
-def compare_numbers(generated_num, selected_num):
-    generated_str = str(generated_num)
-    selected_str = str(selected_num)
-    x_count = 0
-    y_count = 0
-    
-    checked_indices = set()
+game_id = str(uuid.uuid4())
 
-    for digit in range (len(generated_str)):
-        if generated_str[digit] == selected_str[digit]:
-            x_count += 1
-            checked_indices.add(digit)
-    for digit in range(len(generated_str)):
-        if digit not in checked_indices and generated_str[digit] in selected_str:
-            y_count += 1
-    return x_count,y_count
+test.uvitani()
+generated_num = test.generate_unique_four_digit_number()
+
 
 
 while x_count != 4:
@@ -78,9 +33,16 @@ while x_count != 4:
     else:
         print(f"{y_count} Cows")
     print(line)
+    try_count += 1
 
 print(f"Correct, you've guessed the right number")
 print(f"Generated number is: {generated_num}")
+print(f"It takes {try_count} guesses")
+
+
+with open("game_records.txt", mode="a") as txt_file:
+    txt_file.write(f"\n Game ID: {game_id},    Generated number: {generated_num},      Guesses: {try_count}")
+
 
 
 
--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 
 import random
 
+
 ### Uvitaci fce
 def uvitani():
     print("""
@@ -12,6 +13,22 @@ Let's play a bulls and cows game.
 Enter a number:
 -----------------------------------------------
         """  )
+    
+
+
+
+def generate_unique_four_digit_number():
+    first_digit = random.randint(1, 9)
+    other_digits = random.sample([x for x in range(10) if x != first_digit], 3)
+
+    ran_number = first_digit * 1000 + other_digits[0] * 100 + other_digits[1] * 10 + other_digits[2]
+
+    return ran_number
+
+
+
+
+
     
 def correct_number_check():
     smycka = True
@@ -54,5 +71,53 @@ def compare_numbers(generated_num, selected_num):
 
 
 
+
+
 if __name__ == "__main__":
     print("Hello, World!")
+
+
+# def compare_numbers(generated_num, selected_num):
+#     generated_str = str(generated_num)
+#     selected_str = str(selected_num)
+#     x_count = 0
+#     y_count = 0
+    
+#     checked_indices = set()
+
+#     for digit in range (len(generated_str)):
+#         if generated_str[digit] == selected_str[digit]:
+#             x_count += 1
+#             checked_indices.add(digit)
+#     for digit in range(len(generated_str)):
+#         if digit not in checked_indices and generated_str[digit] in selected_str:
+#             y_count += 1
+#     return x_count,y_count
+
+
+# def correct_number_check():
+#     smycka = True
+#     while smycka:
+#         players_number = input("Guess the number: ")
+#         if not players_number.isdigit():
+#             print("You entered non-numeric value.")
+#             continue
+#         elif str(players_number[0]) == "0":
+#             print("Number starts with 0.")
+#             continue
+#         elif len(set(list(str(players_number)))) != 4:
+#             print("Number has less/more than 4 digits or includes duplicates")
+#         else:
+#             smycka = False
+   
+#     print("hope it works")
+#     return players_number
+
+# def generate_unique_four_digit_number():
+#     # Generate a random permutation of digits 1 to 9
+#     digits = random.sample(range(1, 10), 4)
+
+#     # Convert the list of digits to a 4-digit integer
+#     ran_number = digits[0] * 1000 + digits[1] * 100 + digits[2] * 10 + digits[3]
+
+#     return ran_number
