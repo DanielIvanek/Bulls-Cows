7600a0e4491534ff7578c6ed9455b55e8efd0e74
Daniel Ivánek <ivanek.daniel99@gmail.com>
2023-07-31 14:16:43 +0200

    commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Bulls-Cows
+
+# An easy game with numbers. Player chooses 4-digit number a tries to guess random number.
+# Program returns hints in the form Bulls and Cows.
--- main.py
+++ main.py
@@ -7,32 +7,23 @@ discord: Notme#1275
 
 import test
 import uuid
-line = "-" * 30
+import time
+
 try_count = 0
 x_count = 0
 y_count = 0
-
 game_id = str(uuid.uuid4())
+start_time = time.time()
+
 
 test.uvitani()
 generated_num = test.generate_unique_four_digit_number()
 
 
-
 while x_count != 4:
     selected_num = test.correct_number_check()
-    s_num_pr = selected_num
-    x_count,y_count = test.compare_numbers(generated_num,selected_num)
-    print(f">>> {s_num_pr}")
-    if x_count == 1:
-        print(f"{x_count} Bull", end=" ")
-    else:
-        print(f"{x_count} Bulls", end=" ")
-    if y_count == 1:
-        print(f"{y_count} Cow")
-    else:
-        print(f"{y_count} Cows")
-    print(line)
+    x_count, y_count = test.compare_numbers(generated_num, selected_num)
+    test.print_guess_result(selected_num, x_count, y_count)
     try_count += 1
 
 print(f"Correct, you've guessed the right number")
@@ -40,24 +31,9 @@ print(f"Generated number is: {generated_num}")
 print(f"It takes {try_count} guesses")
 
 
+end_time = time.time()
+duration = end_time - start_time
 with open("game_records.txt", mode="a") as txt_file:
-    txt_file.write(f"\n Game ID: {game_id},    Generated number: {generated_num},      Guesses: {try_count}")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    txt_file.write(
+        f"\n Game ID: {game_id},    Generated number: {generated_num},      Guesses: {try_count}         Duration: {duration: .0f} seconds"
+    )
